827445260ec251484cfac085e4993c848f7bc2f6
jac <jacdtaylor@gmail.com>
2024-02-20 14:22:25 -0700

    Completion opacity

--- manager.js
+++ manager.js
@@ -34,7 +34,9 @@ document.addEventListener("DOMContentLoaded", function() {
         title: title,
         date: date,
         completion_value: -1,
-        completion: "Incomplete"
+        completion: "Incomplete",
+        completion_task: "Complete",
+        opacity: 1
       };
       tasks.push(task);
       renderTasks();
@@ -44,13 +46,15 @@ document.addEventListener("DOMContentLoaded", function() {
     function renderTasks() {
       const currentTask = tasks[currentIndex];
       if (currentTask) {
+        const container = document.getElementById("task-container");
+        container.style.opacity = currentTask.opacity;
         taskContainer.innerHTML = `
           <h2 class="task-title">${currentTask.title}</h2>
           <p class="task-date">${currentTask.date}</p>
           <p class="completion-status">${currentTask.completion}</p>
           <button class="delete-task-button">Delete</button>
           <button class="edit-task-button">Edit</button>
-          <button class="complete-task-button">Toggle Completeness</button>
+          <button class="complete-task-button">Mark as ${currentTask.completion_task}</button>
         `;
       } else {
         taskContainer.innerHTML = '<p>No tasks available</p>';
@@ -108,9 +112,16 @@ document.addEventListener("DOMContentLoaded", function() {
           }} else if (event.target.classList.contains('complete-task-button')) {
             const currentTask = tasks[currentIndex];
             currentTask.completion_value *= -1;
+                
             if (currentTask.completion_value == 1) {
+                currentTask.opacity = 0.5
+
+
                 currentTask.completion = "Completed"
+                currentTask.completion_task ="Incomplete"
             } else {
+                currentTask.opacity = 1;
+                currentTask.completion_task ="Complete"
                 currentTask.completion = "Incomplete"
             }
             renderTasks();
