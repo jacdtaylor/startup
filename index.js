f753e9b7a66f4216da57044c6edd9cffdddf6396
jac <jacdtaylor@gmail.com>
2024-03-09 20:06:43 -0700

    Starting from scratch kinda

--- index.js
+++ index.js
@@ -1,8 +1,72 @@
+// const express = require('express');
+// const app = express();
+
+// // Define tasks array before setting up routes
+
+
+// // The service port. In production the front-end code is statically hosted by the service on the same port.
+// const port = process.argv.length > 2 ? process.argv[2] : 3000;
+
+// // JSON body parsing using built-in middleware
+// app.use(express.json());
+
+// // Serve up the front-end static content hosting
+// app.use(express.static('public'));
+
+// // Router for service endpoints
+// var apiRouter = express.Router();
+// app.use(`/api`, apiRouter);
+
+// apiRouter.get('/tasks', (_req, res) => {
+//   res.send(tasks);
+// });
+
+// // Add Task
+// apiRouter.post('/tasks', (req, res) => {
+//     addtask(req.body, tasks);
+//     res.send(tasks);
+// });
+
+
+// apiRouter.delete('/tasks/:index', (req, res) => {
+//     const index = req.params.index;
+//     if (index >= 0 && index < tasks.length) {
+//       tasks.splice(index, 1);
+//       res.send(tasks);
+//     } else {
+//       res.status(404).send("Task not found");
+//     }
+// });
+
+// apiRouter.put('/tasks/:index', (req, res) => {
+//   const index = req.params.index;
+//   if (index >= 0 && index < tasks.length) {
+//       tasks[index] = req.body; // Replace the task at the specified index with the updated task
+//       res.send(tasks[index]);
+//   } else {
+//       res.status(404).send("Task not found");
+//   }
+// });
+// // Return the application's default page if the path is unknown
+// app.use((_req, res) => {
+//   res.sendFile('index.html', { root: 'public' });
+// });
+
+// app.listen(port, () => {
+//   console.log(`Listening on port ${port}`);
+// });
+
+
+// let tasks = []
+// function addtask(newTask, tasks) {
+//   tasks.push(newTask);
+// }
+
+// api.js
+// api.js
+
 const express = require('express');
 const app = express();
-
-// Define tasks array before setting up routes
-
 
 // The service port. In production the front-end code is statically hosted by the service on the same port.
 const port = process.argv.length > 2 ? process.argv[2] : 3000;
@@ -17,36 +81,17 @@ app.use(express.static('public'));
 var apiRouter = express.Router();
 app.use(`/api`, apiRouter);
 
+// Gettasks
 apiRouter.get('/tasks', (_req, res) => {
   res.send(tasks);
 });
 
-// Add Task
-apiRouter.post('/tasks', (req, res) => {
-    addtask(req.body, tasks);
-    res.send(tasks);
+// Submittask
+apiRouter.post('/task', (req, res) => {
+  tasks = updatetasks(req.body, tasks);
+  res.send(tasks);
 });
 
-
-apiRouter.delete('/tasks/:index', (req, res) => {
-    const index = req.params.index;
-    if (index >= 0 && index < tasks.length) {
-      tasks.splice(index, 1);
-      res.send(tasks);
-    } else {
-      res.status(404).send("Task not found");
-    }
-});
-
-apiRouter.put('/tasks/:index', (req, res) => {
-  const index = req.params.index;
-  if (index >= 0 && index < tasks.length) {
-      tasks[index] = req.body; // Replace the task at the specified index with the updated task
-      res.send(tasks[index]);
-  } else {
-      res.status(404).send("Task not found");
-  }
-});
 // Return the application's default page if the path is unknown
 app.use((_req, res) => {
   res.sendFile('index.html', { root: 'public' });
@@ -56,8 +101,26 @@ app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
 
+// updatetasks considers a new task for inclusion in the high tasks.
+// The high tasks are saved in memory and disappear whenever the service is restarted.
+let tasks = [];
+function updatetasks(newtask, tasks) {
+  let found = false;
+  for (const [i, prevtask] of tasks.entries()) {
+    if (newtask.task > prevtask.task) {
+      tasks.splice(i, 0, newtask);
+      found = true;
+      break;
+    }
+  }
 
-let tasks = []
-function addtask(newTask, tasks) {
-  tasks.push(newTask);
+  if (!found) {
+    tasks.push(newtask);
+  }
+
+  if (tasks.length > 10) {
+    tasks.length = 10;
+  }
+
+  return tasks;
 }
--- public/manager.js
+++ public/manager.js
@@ -1,24 +1,67 @@
-// const playerNameEl = document.querySelector('.player-name');
-// playerNameEl.textContent = getPlayerName();
+const playerNameEl = document.querySelector('.player-name');
+playerNameEl.textContent = getPlayerName();
 
-// function getPlayerName() {
-//   return localStorage.getItem('userName') ?? 'guest';
-// }
+function getPlayerName() {
+  return localStorage.getItem('userName') ?? 'guest';
+}
 
-// function InputVisibility() {
-//     var T = document.getElementById("InputFields");
-//     var V = document.getElementById("RevealOptions")
-//     T.style.display = "block";
-//     V.style.display = "none"
-//       // <-- Set it to block
-// }
+function InputVisibility() {
+  var T = document.getElementById("InputFields");
+  var V = document.getElementById("RevealOptions")
+  T.style.display = "block";
+  V.style.display = "none"
+}
 
-// function ReturnVisibility() {
-//     var V = document.getElementById("InputFields");
-//     var T = document.getElementById("RevealOptions")
-//     T.style.display = "block";
-//     V.style.display = "none"
-// }
+function ReturnVisibility() {
+  var V = document.getElementById("InputFields");
+  var T = document.getElementById("RevealOptions")
+  T.style.display = "block";
+  V.style.display = "none"
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+  let tasks = [];
+  createTaskButton = document.querySelector('.create-task-button');
+
+  async function createNewTask(title, date) {
+    const NewTask = {title: title, date: date};
+    try {
+      const response = await fetch('/api/task', {
+        method: 'POST',
+        headers: {'content-type': 'application/json'},
+        body: JSON.stringify(NewTask),
+      });
+      const tasks = await response.json();
+    } catch (error) {
+      console.error('Error creating new task:', error);
+    }
+  }
+
+  async function displayTasks(tasks) {
+    
+  }
+
+
+  async function retrieveTask() {
+    try {
+      // Get the latest high scores from the service
+      const response = await fetch('/api/tasks');
+      tasks= await response.json();
+  
+      // Save the scores in case we go offline in the future
+      localStorage.setItem('tasks', JSON.stringify(tasks));
+    } catch {
+      // If there was an error then just use the last saved scores
+      const tasksText = localStorage.getItem('tasks');
+      if (tasksText) {
+        tasks = JSON.parse(tasksText);
+       }}
+    displayTasks(tasks);
+  }
+});
+
+
+
 
 // document.addEventListener("DOMContentLoaded", function() {
 //     let tasks = [];
@@ -209,188 +252,4 @@
 //   );
 //   loadTasks();
 // })
-
-
-
-
-const playerNameEl = document.querySelector('.player-name');
-playerNameEl.textContent = getPlayerName();
-
-function getPlayerName() {
-  return localStorage.getItem('userName') ?? 'guest';
-}
-
-function InputVisibility() {
-  var T = document.getElementById("InputFields");
-  var V = document.getElementById("RevealOptions");
-  T.style.display = "block";
-  V.style.display = "none";
-}
-
-function ReturnVisibility() {
-  var V = document.getElementById("InputFields");
-  var T = document.getElementById("RevealOptions");
-  T.style.display = "block";
-  V.style.display = "none";
-}
-
-document.addEventListener("DOMContentLoaded", function() {
-  let tasks = [];
-  let currentIndex = 0;
-  const taskContainer = document.getElementById('task-container');
-  const taskTitleInput = document.getElementById('task-title');
-  const taskDateInput = document.getElementById('task-date');
-  const createTaskButton = document.querySelector('.create-task-button');
-
-  async function createTask(title, date) {
-    const newTask = {
-      title: title,
-      date: date,
-      completion_value: -1,
-      completion: "Incomplete",
-      completion_task: "Complete",
-      opacity: 1,
-      owner: getPlayerName()
-    };
-
-    try {
-      const response = await fetch('/api/tasks', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify(newTask)
-      });
-      loadTasks(); // Updated to load tasks after creation
-    } catch (error) {
-      const container = document.getElementById("task-container");
-      container.innerHTML = `
-        <h2>Create Error</h2>
-        <p>${error}</p>
-      `;
-    }
-  }
-
-  async function loadTasks() {
-    try {
-      const response = await fetch("/api/tasks");
-      tasks = await response.json(); // Fixed to await the response.json() call
-      renderTasks();
-    } catch (error) {
-      const container = document.getElementById("task-container");
-      container.innerHTML = `
-        <h2>Load Error</h2>
-        <p>${error}</p>
-      `;
-      const taskText = localStorage.getItem('tasks');
-      if (taskText) {
-        tasks = JSON.parse(taskText);
-      }
-    }
-  }
-
-  async function renderTasks() {
-    try {
-      const currentTask = tasks[currentIndex];
-      if (currentTask) {
-        const container = document.getElementById("task-container");
-        container.style.opacity = currentTask.opacity;
-        container.innerHTML = `
-          <h2 class="task-title">${currentTask.title}</h2>
-          <p class="task-date">${currentTask.date}</p>
-          <p class="completion-status">${currentTask.completion}</p>
-          <button class="delete-task-button">Delete</button>
-          <button class="edit-task-button">Edit</button>
-          <button class="share-task-button">Share</button>
-          <button class="complete-task-button">Mark as ${currentTask.completion_task}</button>
-        `;
-      } else {
-        document.getElementById("task-container").innerHTML = `<p>No tasks available</p>`;
-      }
-    } catch (error) {
-      const container = document.getElementById("task-container");
-      container.innerHTML = `
-        <h2>Render Error</h2>
-        <p>${error}</p>
-      `;
-    }
-  }
-
-  createTaskButton.addEventListener('click', function() {
-    var now = new Date();
-    var datetime = now.toLocaleString();
-    const title = taskTitleInput.value;
-    const date = datetime;
-    if (title && date) {
-      createTask(title, date);
-      taskTitleInput.value = '';
-      ReturnVisibility();
-    } else {
-      alert('Please enter task title.');
-    }
-  });
-
-  const prevButton = document.querySelector('.prev');
-  const nextButton = document.querySelector('.next');
-
-  prevButton.addEventListener('click', function() {
-    currentIndex = (currentIndex - 1 + tasks.length) % tasks.length;
-    renderTasks();
-  });
-
-  nextButton.addEventListener('click', function() {
-    currentIndex = (currentIndex + 1) % tasks.length;
-    renderTasks();
-  });
-
-  taskContainer.addEventListener('click', async function(event) {
-    if (event.target.classList.contains('delete-task-button')) {
-      try {
-        await fetch(`/api/tasks/${currentIndex}`, {
-          method: 'DELETE'
-        });
-        currentIndex = Math.min(currentIndex, tasks.length - 1);
-        loadTasks();
-      } catch (error) {
-        console.error('Error deleting task:', error);
-      }
-    } else if (event.target.classList.contains('edit-task-button')) {
-      const currentTask = tasks[currentIndex];
-      const taskTitle = prompt('Enter new task title:', currentTask.title);
-      const taskDate = prompt('Enter new task date:', currentTask.date);
-      if (taskTitle !== null && taskDate !== null) {
-        currentTask.title = taskTitle;
-        currentTask.date = taskDate;
-        await fetch(`/api/tasks/${currentIndex}`, {
-          method: 'PUT',
-          headers: {
-            'Content-Type': 'application/json'
-          },
-          body: JSON.stringify(currentTask)
-        });
-        loadTasks();
-      }
-    } else if (event.target.classList.contains('complete-task-button')) {
-      const currentTask = tasks[currentIndex];
-      currentTask.completion_value *= -1;
-      if (currentTask.completion_value == 1) {
-        currentTask.opacity = 0.5;
-        currentTask.completion = "Completed";
-        currentTask.completion_task = "Incomplete";
-      } else {
-        currentTask.opacity = 1;
-        currentTask.completion_task = "Complete";
-        currentTask.completion = "Incomplete";
-      }
-      await fetch(`/api/tasks/${currentIndex}`, {
-        method: 'PUT',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(currentTask)
-      });
-      loadTasks();
-    }
-  });
-
-  loadTasks();
-});
 
