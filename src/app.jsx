02d4b78c7e53d46f3c67c0bf961156e8f787d2f5
jac <jacdtaylor@gmail.com>
2024-04-03 09:25:20 -0600

    Working on app.jsx

---
+++ src/app.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './app.css';
+
+export default function App() {
+  return (
+    <div>
+      <header className='container-fluid'>
+        <nav className='navbar fixed-top navbar-dark'>
+          <div className='header'>
+            Colab Task Manager<sup>&reg;</sup>
+          </div>
+          <div className="dropdown">
+            <button className="dropbtn">Menu</button>
+            <div className="dropdown-content">
+              <a href="index.html" onClick={Logout}>Logout</a>
+              <a href="manager.html">Personal Manager</a>
+              <a href="shared.html">Forum</a>
+              <a href="about.html">About</a>
+            </div>
+          </div>
+        </nav>
+      </header>
+
+      <main className='container-fluid'>
+        {/* App components go here */}
+      </main>
+
+      <footer className='container-fluid'>
+        <hr />
+        <div>
+          <span className="text-reset">Jac Taylor</span>
+          <br />
+          <a href="https://github.com/jacdtaylor/startup">GitHub</a>
+        </div>
+      </footer>
+    </div>
+  );
+}
+=
