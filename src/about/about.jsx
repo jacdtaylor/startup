8db22c4a627ab23be7b9d1d955661e016acc23a5
jac <jacdtaylor@gmail.com>
2024-04-04 09:24:40 -0600

    Base Level React Finished

--- src/about/about.jsx
+++ src/about/about.jsx
@@ -1,9 +1,51 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 export function About() {
+  const [pictureUrl, setPictureUrl] = useState('');
+  const [quote, setQuote] = useState('');
+  const [author, setAuthor] = useState('');
+
+  useEffect(() => {
+    displayPicture();
+    displayQuote();
+  }, []);
+
+  function displayPicture() {
+    const random = Math.floor(Math.random() * 1000);
+    fetch(`https://picsum.photos/v2/list?page=${random}&limit=1`)
+      .then((response) => response.json())
+      .then((data) => {
+        const imgUrl = `https://picsum.photos/id/${data[0].id}/500/300?grayscale`;
+        setPictureUrl(imgUrl);
+      });
+  }
+
+  function displayQuote() {
+    fetch('https://api.quotable.io/random')
+      .then((response) => response.json())
+      .then((data) => {
+        setQuote(data.content);
+        setAuthor(data.author);
+      });
+  }
+
   return (
-    <main className='container-fluid bg-secondary text-center'>
-      <div>about displayed here</div>
+    <main>
+      <p>
+        Colab Task Manager is a program to organize and manage tasks individually or share tasks with others to work on projects collaboratively.
+      </p>
+
+      <div className="container">
+        {pictureUrl && (
+          <img src={pictureUrl} alt="Random" className="image" />
+        )}
+        <div className="overlay">
+          <div className="text">
+            {quote && <p className="quote">{quote}</p>}
+            {author && <p className="author">- {author}</p>}
+          </div>
+        </div>
+      </div>
     </main>
   );
 }
--- src/forum/forum.jsx
+++ src/forum/forum.jsx
@@ -1,9 +1,101 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 export function Forum() {
+  const [playerName, setPlayerName] = useState('');
+  const [forum, setForum] = useState('Public');
+  const [messages, setMessages] = useState([]);
+  const [messageInput, setMessageInput] = useState('');
+
+  useEffect(() => {
+    setPlayerName(localStorage.getItem('userName') || 'guest');
+  }, []);
+
+  useEffect(() => {
+    configureWebSocket();
+  }, [forum]);
+
+  let socket;
+
+  const configureWebSocket = () => {
+    const protocol = window.location.protocol === 'http:' ? 'ws' : 'wss';
+    socket = new WebSocket(`${protocol}://${window.location.host}/ws`);
+    socket.onopen = () => {
+      console.log('WebSocket connection established.');
+    };
+
+    socket.onerror = (error) => {
+      console.error('WebSocket error:', error);
+    };
+
+    socket.onmessage = (event) => {
+      const receivedMessage = JSON.parse(event.data);
+      if (receivedMessage.forum === forum) {
+        setMessages((prevMessages) => [...prevMessages, receivedMessage]);
+        console.log("Recieved")
+      }
+    };
+  };
+
+  const sendMessage = () => {
+    
+    const trimmedMessage = messageInput.trim();
+    
+      
+      const newMessage = {
+        from: playerName,
+        message: trimmedMessage,
+        forum: forum
+      };
+      
+      socket.send(JSON.stringify(newMessage));
+     
+      setMessageInput('');
+    
+  };
+
+  const changeForum = () => {
+    const newForum = document.getElementById('forum-input').value.trim();
+    if (newForum !== '') {
+      setForum(newForum);
+      setMessages([]);
+    }
+  };
+
   return (
-    <main className='container-fluid bg-secondary text-center'>
-      <div>forum displayed here</div>
+    <main>
+      <div>
+        <input type="text" id="forum-input" placeholder="Enter new forum..." />
+        <button id="change-forum-btn" onClick={changeForum}>Change Forum</button>
+      </div>
+
+      <br />
+
+      <div id="chat-box">
+        <div id="player-messages">
+          {messages.map((msg, index) => (
+            <div className="event" key={index}>
+              <div className="user-row">
+                <span className={msg.from === playerName ? 'CurrentUser' : 'OtherUser'}>
+                  {msg.from}:
+                </span> {msg.message}
+              </div>
+            </div>
+          ))}
+        </div>
+        <input
+          type="text"
+          id="message-input"
+          placeholder="Type your message..."
+          value={messageInput}
+          onChange={(e) => setMessageInput(e.target.value)}
+          onKeyDown={(e) => {
+            if (e.key === 'Enter') {
+              sendMessage();
+            }
+          }}
+        />
+        <button id="send-button" onClick={sendMessage}>Send</button>
+      </div>
     </main>
   );
 }
