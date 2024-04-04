4537419165383e98394eecb50ffbafcaf66de6b4
jac <jacdtaylor@gmail.com>
2024-04-04 13:06:08 -0600

    forumMessage completed

--- src/forum/forumMessage.js
+++ src/forum/forumMessage.js
@@ -7,4 +7,28 @@ class EventMessage {
     }}
 
 
-    
+class messageNotifier {
+events = [];
+
+constructor() {
+    let port = window.location.port;
+    const protocol = window.location.protocol === 'http:' ? 'ws' : 'wss';
+    this.socket = new WebSocket(`${protocol}://${window.location.hostname}:${port}/ws`);
+    this.socket.onmessage = async (msg) => {
+        try{
+            const event = JSON.parse(await msg.data.text());
+            this.receiveEvent(event);
+        } catch {}};}
+
+broadcastEvent(from, message, forum) {
+    const event = new EventMessage(from, message, forum)
+    this.socket.send(JSON.stringify(event));
+}
+
+receiveEvent(event) {
+    this.events.push(event);
+}
+}
+
+const Notifier = new messageNotifier();
+export {GameNotifier};
